e4ade6420fb921795e1ac0208daa686becd0e68a
C Yo <young.carter@outlook.com>
2024-02-06 10:36:57 -0500

    fixed simulation to make the robot appear
    still need to replace the placeholder values for the differentialDrivetrainSim in order for it to actually work

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -39,12 +39,9 @@ private final CANSparkMax frontLeftMotor;
     private final EncoderSim m_rightEncoderSim = new EncoderSim(m_rightEncoder);
     private final AnalogGyroSim m_gyroSim = new AnalogGyroSim(m_gyro);
     DifferentialDriveOdometry m_odometry = new DifferentialDriveOdometry(m_gyro.getRotation2d(), m_leftEncoder.getDistance(), m_rightEncoder.getDistance(), new Pose2d(5.0, 13.5, new Rotation2d()));
-    DifferentialDrivetrainSim m_driveSim = new DifferentialDrivetrainSim(DCMotor.getNEO(2), 7.29, 7.5, 60.0, Units.inchesToMeters(3), 0.7112, VecBuilder.fill(0.001, 0.001, 0.001, 0.1, 0.1, 0.005, 0.005));
     private Field2d m_field = new Field2d();
 
     public Drivetrain() {
-
-<<<<<<< Updated upstream
         // if the code is runing on the robot, define the motors as brushed, but if it is in the sim, then define them as brusless to prevent the error when simulating the robot
 
         if (Robot.isReal()){
@@ -60,34 +57,6 @@ private final CANSparkMax frontLeftMotor;
             this.backLeftMotor = new CANSparkMax(Constants.MotorCANID.backLeftMotorCANID, CANSparkLowLevel.MotorType.kBrushless);
             this.backRightMotor = new CANSparkMax(Constants.MotorCANID.backRightMotorCANID, CANSparkLowLevel.MotorType.kBrushless);
         }
-        
-=======
-        // if the code is runing on the robot, define the motors as brushed, but if it
-        // is in the sim, then define them as brusless to prevent the error when
-        // simulating the robot
-
-        if (Robot.isReal()) {
-            // Define the motor ports and motor types as brushed
-            this.frontLeftMotor = new CANSparkMax(Constants.MotorCANID.frontLeftMotorCANID,
-                    CANSparkLowLevel.MotorType.kBrushed);
-            this.frontRightMotor = new CANSparkMax(Constants.MotorCANID.frontRightMotorCANID,
-                    CANSparkLowLevel.MotorType.kBrushed);
-            this.backLeftMotor = new CANSparkMax(Constants.MotorCANID.backLeftMotorCANID,
-                    CANSparkLowLevel.MotorType.kBrushed);
-            this.backRightMotor = new CANSparkMax(Constants.MotorCANID.backRightMotorCANID,
-                    CANSparkLowLevel.MotorType.kBrushed);
-        } else {
-            // Define the motor ports and motor types as brushless
-            this.frontLeftMotor = new CANSparkMax(Constants.MotorCANID.frontLeftMotorCANID,
-                    CANSparkLowLevel.MotorType.kBrushless);
-            this.frontRightMotor = new CANSparkMax(Constants.MotorCANID.frontRightMotorCANID,
-                    CANSparkLowLevel.MotorType.kBrushless);
-            this.backLeftMotor = new CANSparkMax(Constants.MotorCANID.backLeftMotorCANID,
-                    CANSparkLowLevel.MotorType.kBrushless);
-            this.backRightMotor = new CANSparkMax(Constants.MotorCANID.backRightMotorCANID,
-                    CANSparkLowLevel.MotorType.kBrushless);
-        }
->>>>>>> Stashed changes
         
         // Link the motors on the right and left
         this.backLeftMotor.follow(frontLeftMotor);
@@ -113,6 +82,9 @@ private final CANSparkMax frontLeftMotor;
 
     // // Update robot position for the sim
     public void simulationPeriodic() {
+
+        // Set the simulation differential drivetrain (placeholder values to change later)
+        DifferentialDrivetrainSim m_driveSim = new DifferentialDrivetrainSim(DCMotor.getNEO(2), 7.29, 7.5, 60.0, Units.inchesToMeters(3), 0.7112, VecBuilder.fill(0.001, 0.001, 0.001, 0.1, 0.1, 0.005, 0.005));
         
     m_leftEncoderSim.setDistance(m_driveSim.getLeftPositionMeters());
     m_leftEncoderSim.setRate(m_driveSim.getLeftVelocityMetersPerSecond());
@@ -120,8 +92,12 @@ private final CANSparkMax frontLeftMotor;
     m_rightEncoderSim.setRate(m_driveSim.getRightVelocityMetersPerSecond());
     m_gyroSim.setAngle(-m_driveSim.getHeading().getDegrees());
 
-    m_odometry.update(m_gyro.getRotation2d(), m_leftEncoder.getDistance(), m_rightEncoder.getDistance());
-    m_field.setRobotPose(m_odometry.getPoseMeters());
     SmartDashboard.putData("Field", m_field);
+    }
+
+    public void Periodic() {
+        // Update the robot position
+        m_odometry.update(m_gyro.getRotation2d(), m_leftEncoder.getDistance(), m_rightEncoder.getDistance());
+        m_field.setRobotPose(m_odometry.getPoseMeters());
     }
 }
