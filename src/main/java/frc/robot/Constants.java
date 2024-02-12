45891cf7a6fe9f74d8c1f9e24075c46e62b4f764
Inuka Silva <Inukasilva@outlook.com>
2024-02-12 00:17:20 -0500

    added theorical auto, cleaned up code

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -13,8 +13,50 @@ package frc.robot;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
+
   public static class OperatorConstants {
+    // Ports
     public static final int kDriverControllerPort = 0;
+    public static final int kOperatorControllerPort = 1;
+
+    // Buttons
+    public static final int A_BUTTON = 1;  
+    public static final int B_BUTTON = 2; 
+    public static final int X_BUTTON = 3;
+    public static final int Y_BUTTON = 4;
+    public static final int LEFT_BUMPER = 5;
+    public static final int RIGHT_BUMPER = 6; 
+    public static final int BACK = 7;
+    public static final int START = 8; 
+    public static final int LEFT_STICK = 9;
+    public static final int RIGHT_STICK = 10;
+
+    public static final int POV_UP = 0;
+    public static final int POV_LEFT = 270;
+    public static final int POV_DOWN = 180;
+    public static final int POV_RIGHT = 90;
+
+    // Axis
+    public static final int LEFT_X = 0;
+    public static final int LEFT_Y = 1;
+    public static final int LEFT_TRIGGER = 2;
+    public static final int RIGHT_TRIGGER = 3;
+    public static final int RIGHT_X = 4;
+    public static final int RIGHT_Y = 5;
+    
+     // Trigger Control
+    public static final double DIGITAL_TRIGGER_THRESHOLD = 0.85;
+
+    public static final double JOYSTICK_DEADBAND= 0.05;
+    public static final double THROTTLE_FACTOR = -1.0;
+    public static final double ROTATION_FACTOR = -0.8;
+  }
+
+  public static class RobotConstants {
+    public static final double GEARBOX_STAGE_1 = 0;
+    public static final double WHEEL_DIAMETER_IN = 0;
+    public static final double GEARBOX_STAGE_2 = 0;
+    public static double ROBOT_WIDTH = 0.0; //put actual value here
   }
 
   public static class MotorCANID {
--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -1,103 +1,194 @@
 package frc.robot.subsystems;
 
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.util.HolonomicPathFollowerConfig;
+import com.pathplanner.lib.util.PIDConstants;
+import com.pathplanner.lib.util.ReplanningConfig;
 import com.revrobotics.CANSparkLowLevel;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
 
+import edu.wpi.first.hal.AllianceStationID;
 import edu.wpi.first.math.VecBuilder;
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
 import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
+import edu.wpi.first.math.kinematics.DifferentialDriveWheelSpeeds;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 import edu.wpi.first.wpilibj.AnalogGyro;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorController;
 import edu.wpi.first.wpilibj.simulation.AnalogGyroSim;
 import edu.wpi.first.wpilibj.simulation.DifferentialDrivetrainSim;
 import edu.wpi.first.wpilibj.simulation.EncoderSim;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.MotorCANID;
+import frc.robot.Constants.RobotConstants;
 import frc.robot.Robot;
 
 public class Drivetrain extends SubsystemBase{
 
-private final CANSparkMax frontLeftMotor;
-  private final CANSparkMax frontRightMotor;
-  private final CANSparkMax backLeftMotor;
-  private final CANSparkMax backRightMotor;
+    private final CANSparkMax m_frontLeftMotor = new CANSparkMax(MotorCANID.DrivetrainID.frontLeftMotorCANID, 
+    CANSparkLowLevel.MotorType.kBrushless);
+    private final CANSparkMax m_frontRightMotor = new CANSparkMax(MotorCANID.DrivetrainID.frontRightMotorCANID, 
+    CANSparkLowLevel.MotorType.kBrushless);
 
-  private final DifferentialDrive drive;
+    private final CANSparkMax m_backLeftMotor = new CANSparkMax(MotorCANID.DrivetrainID.backLeftMotorCANID, 
+    CANSparkLowLevel.MotorType.kBrushless);
+    private final CANSparkMax m_backRightMotor = new CANSparkMax(MotorCANID.DrivetrainID.backRightMotorCANID, 
+    CANSparkLowLevel.MotorType.kBrushless);
 
-    // Simulated objects for the sim odometry
-    private final Encoder m_leftEncoder = new Encoder(1, 2);
-    private final Encoder m_rightEncoder = new Encoder(3, 4);
-    private final AnalogGyro m_gyro = new AnalogGyro(1);
-    private final EncoderSim m_leftEncoderSim = new EncoderSim(m_leftEncoder);
-    private final EncoderSim m_rightEncoderSim = new EncoderSim(m_rightEncoder);
-    private final AnalogGyroSim m_gyroSim = new AnalogGyroSim(m_gyro);
-    DifferentialDriveOdometry m_odometry = new DifferentialDriveOdometry(m_gyro.getRotation2d(), m_leftEncoder.getDistance(), m_rightEncoder.getDistance(), new Pose2d(5.0, 13.5, new Rotation2d()));
+    private final RelativeEncoder m_left = m_frontLeftMotor.getEncoder();
+    private final RelativeEncoder m_right = m_frontRightMotor.getEncoder();
+
+    private final ADXRS450_Gyro m_gyro = new ADXRS450_Gyro();
+
+    private final DifferentialDriveOdometry m_odometry = new DifferentialDriveOdometry(
+        m_gyro.getRotation2d(),
+        getLeftEncoderMeters(),
+        getRightEncoderMeters()
+    );
+
+    private final DifferentialDrive m_drive = new DifferentialDrive(
+        m_frontLeftMotor::set, 
+        m_frontRightMotor::set
+    );
+
+    private final DifferentialDriveKinematics m_kinematics = new DifferentialDriveKinematics(
+        Units.inchesToMeters(RobotConstants.ROBOT_WIDTH)
+    );
+
     private Field2d m_field = new Field2d();
 
+
+    private final Boolean m_isRedAlliance;
+
+    private final PIDController m_leftPIDController = new PIDController(1, 0, 0);
+    private final PIDController m_rightPIDController = new PIDController(1, 0, 0);
+
     public Drivetrain() {
-        // if the code is runing on the robot, define the motors as brushed, but if it is in the sim, then define them as brusless to prevent the error when simulating the robot
 
-        if (Robot.isReal()){
-            // Define the motor ports and motor types as brushed
-            this.frontLeftMotor = new CANSparkMax(MotorCANID.DrivetrainID.frontLeftMotorCANID, CANSparkLowLevel.MotorType.kBrushed);
-            this.frontRightMotor = new CANSparkMax(MotorCANID.DrivetrainID.frontRightMotorCANID, CANSparkLowLevel.MotorType.kBrushed);
-            this.backLeftMotor = new CANSparkMax(MotorCANID.DrivetrainID.backLeftMotorCANID, CANSparkLowLevel.MotorType.kBrushed);
-            this.backRightMotor = new CANSparkMax(MotorCANID.DrivetrainID.backRightMotorCANID, CANSparkLowLevel.MotorType.kBrushed);
-        } else {
-            // Define the motor ports and motor types as brushless
-            this.frontLeftMotor = new CANSparkMax(MotorCANID.DrivetrainID.frontLeftMotorCANID, CANSparkLowLevel.MotorType.kBrushless);
-            this.frontRightMotor = new CANSparkMax(MotorCANID.DrivetrainID.frontRightMotorCANID, CANSparkLowLevel.MotorType.kBrushless);
-            this.backLeftMotor = new CANSparkMax(MotorCANID.DrivetrainID.backLeftMotorCANID, CANSparkLowLevel.MotorType.kBrushless);
-            this.backRightMotor = new CANSparkMax(MotorCANID.DrivetrainID.backRightMotorCANID, CANSparkLowLevel.MotorType.kBrushless);
-        }
-        
+        m_frontLeftMotor.setInverted(true);
+        m_backLeftMotor.setInverted(true);
+
         // Link the motors on the right and left
-        this.backLeftMotor.follow(frontLeftMotor);
-        this.backRightMotor.follow(frontRightMotor);
+        m_backLeftMotor.follow(m_frontLeftMotor);
+        m_backRightMotor.follow(m_frontRightMotor);
+        
+        m_isRedAlliance = isRed( DriverStation.getRawAllianceStation());
 
-        // Set the differential drivetrain
-        this.drive = new DifferentialDrive(frontLeftMotor, frontRightMotor);
+        configureAutoBuilder();
+    }
 
-        // Update simualtion field data
+   
+    public void Periodic() {
+        m_odometry.update(null, getRightEncoderMeters(), getLeftEncoderMeters())
+        m_field.setRobotPose(getPose());
         SmartDashboard.putData("Field", m_field);
     }
 
-    /**
-     * Runs the robot in arcade drive mode at the given speed and with the rotation
-     * @param forward
-     * @param rotation
-     */
-    public Command drive(double forward, double rotation) {
-        return runEnd(() -> {
-            this.drive.arcadeDrive(forward, rotation);
-        }, this.drive::stopMotor);
+    public void arcadeDrive(double throttle, double rotation) {
+        m_drive.arcadeDrive(throttle, rotation);
     }
 
-    // // Update robot position for the sim
-    public void simulationPeriodic() {
-
-        // Set the simulation differential drivetrain (placeholder values to change later)
-        DifferentialDrivetrainSim m_driveSim = new DifferentialDrivetrainSim(DCMotor.getNEO(2), 7.29, 7.5, 60.0, Units.inchesToMeters(3), 0.7112, VecBuilder.fill(0.001, 0.001, 0.001, 0.1, 0.1, 0.005, 0.005));
+    public void driveWithChassisSpeeds(ChassisSpeeds speeds){
+        double x = speeds.vxMetersPerSecond;
+        double y = speeds.vyMetersPerSecond;
+        double rot = speeds.omegaRadiansPerSecond;
         
-    m_leftEncoderSim.setDistance(m_driveSim.getLeftPositionMeters());
-    m_leftEncoderSim.setRate(m_driveSim.getLeftVelocityMetersPerSecond());
-    m_rightEncoderSim.setDistance(m_driveSim.getRightPositionMeters());
-    m_rightEncoderSim.setRate(m_driveSim.getRightVelocityMetersPerSecond());
-    m_gyroSim.setAngle(-m_driveSim.getHeading().getDegrees());
-
-    SmartDashboard.putData("Field", m_field);
+        var wheelSpeeds = m_kinematics.toWheelSpeeds(new ChassisSpeeds(x, y, rot));
+        setSpeeds(wheelSpeeds);
     }
 
-    public void Periodic() {
-        // Update the robot position
-        m_odometry.update(m_gyro.getRotation2d(), m_leftEncoder.getDistance(), m_rightEncoder.getDistance());
-        m_field.setRobotPose(m_odometry.getPoseMeters());
+    public void setSpeeds(DifferentialDriveWheelSpeeds speeds) {
+        final double leftOutput =
+            m_leftPIDController.calculate(getLeftEncoderMetersPerSecond(), speeds.leftMetersPerSecond);
+        final double rightOutput =
+            m_rightPIDController.calculate(getRightEncoderMetersPerSecond(), speeds.rightMetersPerSecond);
+        m_frontLeftMotor.setVoltage(leftOutput);
+        m_frontRightMotor.setVoltage(rightOutput);
+      }
+
+    public Pose2d getPose(){
+        return m_odometry.getPoseMeters();
     }
+
+    public void resetOdometry(Pose2d pose) {
+        m_odometry.resetPosition(m_gyro.getRotation2d(), getRightEncoderMeters(), getLeftEncoderMeters(), pose);
+    }
+
+    private ChassisSpeeds getWheelSpeeds(){
+        var wheelSpeeds = new DifferentialDriveWheelSpeeds(
+        getLeftEncoderMetersPerSecond(), 
+        getRightEncoderMetersPerSecond()
+        );
+        return m_kinematics.toChassisSpeeds(wheelSpeeds);
+    }
+
+    private double getLeftEncoderMetersPerSecond(){
+       return rpmToMeterPerSecond(m_left.getVelocity());
+    }
+    
+    private double getRightEncoderMetersPerSecond(){
+        return rpmToMeterPerSecond(m_right.getVelocity());
+    }
+    
+    private double rpmToMeterPerSecond(double motorRPM){
+        double motorRPS = motorRPM/60;
+        double wheelRPS = motorRPS * RobotConstants.GEARBOX_STAGE_1 * RobotConstants.GEARBOX_STAGE_2 * RobotConstants.PULLEY_STAGE;
+        double distancePerRev = Units.inchesToMeters(RobotConstants.WHEEL_DIAMETER_IN) * Math.PI;
+        return wheelRPS * distancePerRev;
+    }
+
+    public double getLeftEncoderMeters(){
+        return revolutionsToMeters(m_left.getPosition());
+    }
+    public double getRightEncoderMeters(){
+        return revolutionsToMeters(m_right.getPosition());
+    }
+
+    private double revolutionsToMeters(double motorRotations){
+        double wheelRotations = motorRotations * RobotConstants.GEARBOX_STAGE_1 * RobotConstants.GEARBOX_STAGE_2 * RobotConstants.PULLEY_STAGE;
+        double distancePerRevolution = Units.inchesToMeters(RobotConstants.WHEEL_DIAMETER_IN) * Math.PI;
+        return wheelRotations * distancePerRevolution;
+    }
+
+    /* Creates a pathplanner autobuilder for autonomous pathing */
+    public void configureAutoBuilder() {
+        AutoBuilder.configureRamsete(
+        this::getPose, 
+        this::resetOdometry, 
+        this::getWheelSpeeds,
+        this::driveWithChassisSpeeds, 
+        new ReplanningConfig(),
+        this::flipPath, 
+        this);
+    }
+
+    private boolean flipPath(){
+        if (m_isRedAlliance == true){
+          return true;
+        }
+        return false;
+    }
+
+    private boolean isRed(AllianceStationID id){
+      if(id == AllianceStationID.Red1) return true;
+      if(id == AllianceStationID.Red2) return true;
+      if(id == AllianceStationID.Red3) return true;
+      return false;
+
+    }
+  
 }
